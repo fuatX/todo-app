698555ca7385cf68db96869222b0be54ecd8f2e3
fuatX <fuatyagizaydin@hotmail.com>
2023-07-13 19:40:12 +0300

    eventListener ile btn tanımlama

--- todo.js
+++ todo.js
@@ -1,38 +1,50 @@
+"use strict";
+
 let sonuc;
 
-let gorevListesi = ["Görev 1", "Görev 2", "Görev 3", "Görev 4"];
+let gorevListesi = [
+  { "id": 1, "gorevAdi": "Görev 1" },
+  { "id": 2, "gorevAdi": "Görev 2" },
+  { "id": 3, "gorevAdi": "Görev 3" },
+  { "id": 4, "gorevAdi": "Görev 4" },
+]; // value için quotes olmaz
 
 let ul = document.getElementById("task-list");
 
-// ul.children[0].children[0].children[0].checked = true;
-
-// let count = ul.children.length;
-
-for (let index in gorevListesi) {
+for (let gorev of gorevListesi) {
   let li = `
-     <li class="task list-group-item">
-         <div class="form-check">
-           <input type="checkbox" id="${index + 1}" class="form-check-input" />
-            <label for="${index + 1}" class="form-check-label">${
-    gorevListesi[index]
-  }</label>
-         </div>
+    <li class="task list-group-item">
+      <div class="form-check">
+       <input type="checkbox" id="${gorev.id}" class="form-check-input" />
+       <label for="${gorev.id}" class="form-check-label">${gorev.gorevAdi}</label>
+      </div>
      </li>
          `;
 
   ul.insertAdjacentHTML("beforeend", li); // google da sıralamarı var lazımsa ordan bak. bu şekilde yerleştirebiliriz.
 }
+/*
+let btnEkle = document.querySelector("#btnAddNewTask")
 
-/* alternative way with of operator.
-for (let element of gorevListesi) {
-    let li = `
-      <li class="task list-group-item">
-        <div class="form-check">
-          <input type="checkbox" id="${element}" class="form-check-input" />
-          <label for="${element}" class="form-check-label">${element}</label>
-        </div>
-      </li>
-    `;
-    ul.insertAdjacentHTML("beforeend", li);
-  }
+btnEkle.addEventListener("click",function(event){
+  console.log("click event")
+
+  event.preventDefault();
+})
 */
+
+/*
+document.querySelector("#btnClear").addEventListener("click",function(event){
+  console.log("click temizle")
+
+  event.preventDefault();
+})
+*/
+
+document.querySelector("#btnAddNewTask").addEventListener("click", NewTask);
+
+function NewTask(event) {
+  event.target.classList.add("btn-success");
+
+  event.preventDefault();
+}
